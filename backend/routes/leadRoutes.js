550f3a6dc4777f2af374d2f936c508dd7370ab23
unknown <shrivasaditya21@gmail.com>
2026-08-08 13:34:26 +0530

    Remove old project files

--- backend/routes/leadRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getLeadRecommendations } from "../controllers/leadController.js";
-
-const router = express.Router();
-
-router.get("/recommendations/:hoardingId", getLeadRecommendations);
-
-export default router;
